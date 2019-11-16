d7d4736269770de9c14fc42009e1ed5b3b08c53a
mikheev-hub <pavel-laz2010@mail.ru>
2019-11-17 00:20:09 +0300

    Update

--- src/stm32f10x_exti.c
+++ src/stm32f10x_exti.c
@@ -5,21 +5,21 @@ void extiBebin()
 {
     RCC->APB2ENR    |=  RCC_APB2ENR_AFIOEN;
     RCC->APB2ENR    |=  RCC_APB2ENR_IOPAEN;
-	  
-	  EXTI->FTSR      |= EXTI_FTSR_TR0;
-  	EXTI->RTSR      &=~  EXTI_RTSR_TR0;
-	  AFIO->EXTICR[0] |=  AFIO_EXTICR1_EXTI0_PA;
-	
-	  EXTI->PR        |= EXTI_PR_PR0;						
-	  EXTI->IMR       |= EXTI_IMR_MR0;							
-	  EXTI->EMR       |= EXTI_EMR_MR0;
-	
-	  NVIC_EnableIRQ(EXTI0_IRQn);
+
+    EXTI->FTSR      |=  EXTI_FTSR_TR0;
+    EXTI->RTSR      &=~ EXTI_RTSR_TR0;
+    AFIO->EXTICR[0] |=  AFIO_EXTICR1_EXTI0_PA;
+
+    EXTI->PR        |=  EXTI_PR_PR0;						
+    EXTI->IMR       |=  EXTI_IMR_MR0;							
+    EXTI->EMR       |=  EXTI_EMR_MR0;
+
+    NVIC_EnableIRQ(EXTI0_IRQn);
 }
 
 void EXTI0_IRQHandler(void) 
 {  
-    EXTI->PR |= (1<<0);
+    EXTI->PR   |= EXTI_PR_PR0;
 	  GPIOC->ODR ^= 0x0100;
 	  
 }
